976ad6a70792fea25720a6382b3d430f022dee4e
Anna Andriyanova <phyrisrose@gmail.com>
2025-12-05 12:53:02 -0700

    Revert "trying to get around the https restriction"
    This reverts commit b0ed794371be75be1f809f76a7a5180ab4bc052a.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import mkcert from "vite-plugin-mkcert";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -10,11 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-    mkcert(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
